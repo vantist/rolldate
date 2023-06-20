d09089df10604a341b912561f34b6882f1107dda
Vantist <s7790428@gmail.com>
2023-06-20 10:56:08 +0800

    chore: update banner

--- config/banner.js
+++ config/banner.js
@@ -5,7 +5,7 @@ var pkg = require('../package.json'),
   version = pkg.version,
   released = 'Licensed under MIT',
   repository = pkg.repository.url,
-  author = 'Copyright ' + (new Date()).getFullYear() + ' 雾空',
+  author = 'Copyright ' + (new Date()).getFullYear() + ' Vantist',
   date = 'Released on: aug 4, 2018';
 
 module.exports = {
